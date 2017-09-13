b0bc7efd490f1b6e98717dc3a44e2283ff725930
Isgeny <jack091196@gmail.com>
2017-09-13 14:19:44 +0700

    Add map center to coords transformation

--- Map.cs
+++ Map.cs
@@ -12,6 +12,10 @@ namespace MiniGIS
 {
     public partial class Map : UserControl
     {
+        public List<Layer> Layers { get; set; }
+        public double MapScale { get; set; }
+        public GEOPoint MapCenter { get; set; }
+
         public Map()
         {
             InitializeComponent();
@@ -19,11 +23,7 @@ namespace MiniGIS
             Layers = new List<Layer>();
             MapScale = 1.0;
             MapCenter = new GEOPoint();
-        }
-
-        public List<Layer> Layers { get; set; }
-        public double MapScale { get; set; }
-        public GEOPoint MapCenter { get; set; }
+        }       
 
         private void Map_Paint(object sender, PaintEventArgs e)
         {
@@ -33,16 +33,16 @@ namespace MiniGIS
         public System.Drawing.Point MapToScreen(GEOPoint mapPoint)
         {
             var screenPoint = new System.Drawing.Point();
-            screenPoint.X = this.Width / 2 + Convert.ToInt32(mapPoint.X);
-            screenPoint.Y = this.Height / 2 - Convert.ToInt32(mapPoint.Y);
+            screenPoint.X = this.Width / 2 + Convert.ToInt32(mapPoint.X) + Convert.ToInt32(MapCenter.X);
+            screenPoint.Y = this.Height / 2 - Convert.ToInt32(mapPoint.Y) + Convert.ToInt32(MapCenter.Y);
             return screenPoint;
         }
 
         public GEOPoint ScreenToMap(System.Drawing.Point screenPoint)
         {
             var mapPoint = new GEOPoint();
-            mapPoint.X = screenPoint.X - this.Width / 2;
-            mapPoint.Y = this.Height / 2 - screenPoint.Y;
+            mapPoint.X = screenPoint.X - this.Width / 2 - MapCenter.X;
+            mapPoint.Y = this.Height / 2 - screenPoint.Y - MapCenter.Y;
             return mapPoint;
         }
     }
