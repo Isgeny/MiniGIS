7c1a0c931060b3174ed8956ff8ec9a71bee0c8a7
Isgeny <jack091196@gmail.com>
2017-09-21 20:50:02 +0700

    Add toolstrip buttons

--- Form.Designer.cs
+++ Form.Designer.cs
@@ -29,7 +29,15 @@
         private void InitializeComponent()
         {
             MiniGIS.GEOPoint geoPoint1 = new MiniGIS.GEOPoint();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
             this.mapControl = new MiniGIS.Map();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.selectBtn = new System.Windows.Forms.ToolStripButton();
+            this.panBtn = new System.Windows.Forms.ToolStripButton();
+            this.zoomInBtn = new System.Windows.Forms.ToolStripButton();
+            this.zoomOutBtn = new System.Windows.Forms.ToolStripButton();
+            this.entireViewBtn = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // mapControl
@@ -43,22 +51,92 @@
             this.mapControl.Size = new System.Drawing.Size(758, 729);
             this.mapControl.TabIndex = 0;
             // 
+            // toolStrip
+            // 
+            this.toolStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectBtn,
+            this.panBtn,
+            this.zoomInBtn,
+            this.zoomOutBtn,
+            this.entireViewBtn});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(782, 27);
+            this.toolStrip.TabIndex = 1;
+            this.toolStrip.Text = "toolStrip1";
+            // 
+            // selectBtn
+            // 
+            this.selectBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.selectBtn.Image = ((System.Drawing.Image)(resources.GetObject("selectBtn.Image")));
+            this.selectBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.selectBtn.Name = "selectBtn";
+            this.selectBtn.Size = new System.Drawing.Size(24, 24);
+            this.selectBtn.Text = "Select";
+            // 
+            // panBtn
+            // 
+            this.panBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.panBtn.Image = ((System.Drawing.Image)(resources.GetObject("panBtn.Image")));
+            this.panBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.panBtn.Name = "panBtn";
+            this.panBtn.Size = new System.Drawing.Size(24, 24);
+            this.panBtn.Text = "Pan";
+            // 
+            // zoomInBtn
+            // 
+            this.zoomInBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.zoomInBtn.Image = ((System.Drawing.Image)(resources.GetObject("zoomInBtn.Image")));
+            this.zoomInBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.zoomInBtn.Name = "zoomInBtn";
+            this.zoomInBtn.Size = new System.Drawing.Size(24, 24);
+            this.zoomInBtn.Text = "Zoom In";
+            // 
+            // zoomOutBtn
+            // 
+            this.zoomOutBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.zoomOutBtn.Image = ((System.Drawing.Image)(resources.GetObject("zoomOutBtn.Image")));
+            this.zoomOutBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.zoomOutBtn.Name = "zoomOutBtn";
+            this.zoomOutBtn.Size = new System.Drawing.Size(24, 24);
+            this.zoomOutBtn.Text = "Zoom Out";
+            // 
+            // entireViewBtn
+            // 
+            this.entireViewBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.entireViewBtn.Image = ((System.Drawing.Image)(resources.GetObject("entireViewBtn.Image")));
+            this.entireViewBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.entireViewBtn.Name = "entireViewBtn";
+            this.entireViewBtn.Size = new System.Drawing.Size(24, 24);
+            this.entireViewBtn.Text = "Entire View";
+            // 
             // Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.toolStrip);
             this.Controls.Add(this.mapControl);
             this.Name = "Form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MiniGIS";
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Map mapControl;
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton selectBtn;
+        private System.Windows.Forms.ToolStripButton panBtn;
+        private System.Windows.Forms.ToolStripButton zoomInBtn;
+        private System.Windows.Forms.ToolStripButton zoomOutBtn;
+        private System.Windows.Forms.ToolStripButton entireViewBtn;
     }
 }
 
